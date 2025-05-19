9131bbd0291ed32d384e4288e1625f51114f0a45
Shamil Asadov <shamil.asadov.2000@gmail.com>
2025-05-19 22:59:30 +0400

    Added city model and city api key example

---
+++ lib/models/city_model.dart
@@ -0,0 +1,23 @@
+//city model. used in a list return of cities_service.
+class City {
+  final String name;
+  final String country;
+  final double latitude;
+  final double longitude;
+
+  City({
+    required this.name,
+    required this.country,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory City.fromJson(Map<String, dynamic> json) {
+    return City(
+      name: json['name'],
+      country: json['country'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+    );
+  }
+}
